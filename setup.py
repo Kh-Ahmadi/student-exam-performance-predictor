b2b041f852c6945d9f145f1dc0c370a60d929fe1
kh-ahmadi <voiceofkakhan@gmail.com>
2023-03-22 19:39:10 +0500

    setup done

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        reqs = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in reqs]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+
+setup(
+    name='mlstudentperformance',
+    version='0.0.1',
+    author='kahmadi',
+    author_email='voiceofkakhan@gmail.com',
+    packages=find_packages(),
+    requires=get_requirements('requirements.txt'),
+)
